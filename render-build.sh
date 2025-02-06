ca0bdd783d56590e690ee65fd2cfd7186984a77f
macautomation <37091741+macautomation@users.noreply.github.com>
2025-02-06 15:35:20 -0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,12 +3,13 @@ set -o errexit  # Exit on error
 
 npm install
 
-# Store/pull Puppeteer cache with build cache
-if [[ -d $PUPPETEER_CACHE_DIR ]]; then
+# Ensure Puppeteer cache directory exists before copying
+if [[ -d $PUPPETEER_CACHE_DIR && "$(ls -A $PUPPETEER_CACHE_DIR)" ]]; then
   echo "...Copying Puppeteer Cache from Build Cache"
   cp -R $PUPPETEER_CACHE_DIR/* $XDG_CACHE_HOME/puppeteer/
 else
-  echo "...Storing Puppeteer Cache in Build Cache"
+  echo "...Puppeteer Cache Not Found. Running Install..."
   mkdir -p $PUPPETEER_CACHE_DIR
+  npm rebuild puppeteer-core chrome-aws-lambda
   cp -R $XDG_CACHE_HOME/puppeteer/* $PUPPETEER_CACHE_DIR/
 fi
