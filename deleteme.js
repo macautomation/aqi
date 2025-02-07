d52af92184d6303afe7bee8f696dacd5569754ba
macautomation <37091741+macautomation@users.noreply.github.com>
2025-02-06 17:08:31 -0800

    Update deleteme.js

--- deleteme.js
+++ deleteme.js
@@ -1,7 +1,1 @@
-const { join } = require('path');
 
-/** @type {import("puppeteer").Configuration} */
-module.exports = {
-  // Specify the directory where Puppeteer will store browser binaries.
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
